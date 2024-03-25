696833e5b8b27cd40baa55a77c109032de6d47ed
Tyler Scaringella <77750917+TylerScaringella@users.noreply.github.com>
2024-03-25 16:48:50 -0400

    Update order of education

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,13 @@ const education = [
     school: 'Duke University',
     start: 'August 2024',
     end: 'May 2028',
-    degree: 'Bachelor of Science in Economics'
+    degree: 'Bachelor of Science in Computer Science'
   },
   {
     school: 'Duke University',
     start: 'August 2024',
     end: 'May 2028',
-    degree: 'Bachelor of Science in Computer Science'
+    degree: 'Bachelor of Science in Economics'
   },
   {
     school: 'St. Mark\'s School',
