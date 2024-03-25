460ba3d8a4e6b41e6b2216a90c5a39702026cecc
TylerScaringella <tylerscaringella@stmarksschool.org>
2024-03-25 00:58:08 -0400

    initial commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar"
+import Link from "next/link"
+
+export const metadata = {
+    title: "Contact | Tyler Scaringella",
+    description: "Tyler Scaringella Contact Methods",
+}
+
+const contacts = [
+    { href: 'www.linkedin.com/in/tyler-scaringella', text: 'LinkedIn - linkedin.com/in/tyler-scaringella' },
+    { href: 'https://github.com.com/TylerScaringella', text: 'GitHub - TylerScaringella' },
+    { href: 'https://tylerscaringella.com', text: 'Website - tylerscaringella.com' },
+    { href: 'mailto:tyler.scaringella@duke.edu', text: 'Email - tyler.scaringella@duke.edu' }
+]
+
+export default function Contact() {
+    return (
+        <main>
+            <Navbar />
+            <p className="mb-1 font-bold text-lg">🔗 Tyler Scaringella</p>
+            { contacts.map(contact => <p key={contact.href} className="hover:underline"><Link href={contact.href}>{ contact.text }</Link></p>)}
+        </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative mb-3">
+            <ul className="flex flex-row space-x-0 pr-10">
+                <li className="inline-flex items-center gap-x-3 pr-3 text-sm font-medium">
+                    <Link href={'/'}>home</Link>
+                </li>
+                {/* <li className="inline-flex items-center gap-x-3 p-3 px-4 text-sm font-medium">
+                    <Link href={'/resume'}>resume</Link>
+                </li> */}
+                <li className="inline-flex items-center gap-x-3 p-3 px-4 text-sm font-medium">
+                    <Link href={'/contact'}>contact</Link>
+                </li>
+            </ul>
+      </nav>
+    )
+}
