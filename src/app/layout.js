391c5c739bbfc5aa7c9df6d21f896f91ddecdc37
Tyler Scaringella <77750917+TylerScaringella@users.noreply.github.com>
2024-03-31 23:06:49 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 bg-stone-950 lg:mx-auto ${inter.className}`} >{children}</body>
     </html>
   );
